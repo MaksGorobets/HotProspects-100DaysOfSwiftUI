88362c75e9c26d1c45e566feace42ad83f1c997b
MaksGorobets <gorobets542@gmail.com>
2024-02-03 17:44:49 +0200

    Better code refactor

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -13,56 +13,52 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            StarView(selectedTab: $selectedTab)
+            ExampleView(selectedTab: $selectedTab,
+                        gradientColor: .yellow,
+                        text: "Star", image: "star.fill",
+                        buttonText: "Go to Globe view >", 
+                        buttonValue: "GlobeView")
                 .tabItem { Label("Star", systemImage: "star") }
                 .tag("StarView")
-            GlobeView(selectedTab: $selectedTab)
+            ExampleView(selectedTab: $selectedTab,
+                        gradientColor: .blue,
+                        text: "Globe", image: "globe",
+                        buttonText: "< Go to Star view",
+                        buttonValue: "StarView")
                 .tabItem { Label("Globe", systemImage: "globe") }
                 .tag("GlobeView")
         }
     }
 }
 
-struct StarView: View {
+struct ExampleView: View {
     
     @Binding var selectedTab: String
     
+    let gradientColor: Color
+    
+    let text: String
+    let image: String
+    let buttonText: String
+    let buttonValue: String
+    
     var body: some View {
         ZStack {
-            RadialGradient(colors: [.yellow, .white], center: .center, startRadius: 50, endRadius: 200)
+            RadialGradient(colors: [gradientColor, .white], center: .center, startRadius: 50, endRadius: 200)
                 .ignoresSafeArea()
             VStack {
-                Image(systemName: "star.fill")
-                Text("Star")
-                Button("Go to Globe view >") {
-                    selectedTab = "GlobeView"
+                Image(systemName: image)
+                Text(text)
+                Button(buttonText) {
+                    selectedTab = buttonValue
                 }
                 .font(.system(size: 20))
                 .buttonStyle(.plain)
             }
+            .foregroundStyle(.black)
             .font(.system(size: 50))
         }
-    }
-}
-
-struct GlobeView: View {
-    
-    @Binding var selectedTab: String
-    
-    var body: some View {
-        ZStack {
-            RadialGradient(colors: [.blue, .white], center: .center, startRadius: 50, endRadius: 200)
-            VStack {
-                Image(systemName: "globe")
-                Text("Globe")
-                Button("< Go to Star view") {
-                    selectedTab = "StarView"
-                }
-                .font(.system(size: 20))
-                .buttonStyle(.plain)
-            }
-            .font(.system(size: 50))
-        }
+        .preferredColorScheme(.light)
     }
 }
 
